eeee829ac54dde42b2a81d2b64ab4fe53450ac28
anderger <andersge92@gmail.com>
2016-11-04 13:51:06 +0100

    update to current version of AST1100SolarSystem and the given lines for lander

--- satellite_launch.py
+++ satellite_launch.py
@@ -1,4 +1,9 @@
 
 from AST1100ShortcutSystem import AST1100SolarSystem
 
-seed = open(myseed.txt, "rf")
+infile = open('myseed.txt', "rf")
+seed = int(infile.readline())
+myss = AST1100SolarSystem(seed) 
+myss.landOnPlanet(1, 'lander.txt')
+#try: update AST1100SolarSystem.
+
