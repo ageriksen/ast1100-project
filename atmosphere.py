7a1e5bfb09dd95da0dbe3f55d4f889f54ee00d6f
anders <andersge92@gmail.com>
2016-11-09 23:16:20 +0100

    trying to make sense of the strange numbers. the initial step puts the pressure from 1.5 Nm^-2 to ~ -10^7 Nm^-2. The program also overflows if I have too few timesteps.

--- atmosphere.py
+++ atmosphere.py
@@ -17,19 +17,13 @@ gamma = 1.4 # adiabatic power
 M_sol = 1.98855e30 #kg in 1 solar mass
 
 myss = MSS.Myseed()
-#homeplanet is planet 0
-#mass of planets, [solar masses]
-#radi of planets, [km]
-#P[0] 1.40867748005
-#T[0] 281.0
-#r[0] 9327.65184279
-#M 9.03592158922e-06
 
 M = MSS.p_mass(myss, 1) * M_sol # converting to kg
 R = MSS.p_radius(myss, 1)*1e3
 
-nr = 1000000 #1e6 number of steps
-r = np.linspace(R,200e3, nr) #m | distance from surface to end 
+nr = 1000000 #1000000 #1e6 number of steps
+r = np.linspace(R,R+300e3, nr) #m | distance from surface to end 
+dr = 200e3/nr
 P = np.zeros(nr)
 T = np.zeros(nr)
 rho = np.zeros(nr)
@@ -38,24 +32,25 @@ P[0] = myss.rho0[1] # Atmospheric density at surface
 adia = P[0]**(1-gamma)*T[0]**gamma #adiabatic constant
 
 
+print 'dr', dr
 print 'P[0]', P[0]
 print 'T[0]', T[0]
 print 'r[0]', r[0]
 print 'M', M
 
 for i in range(nr-1):
-    print i # stops at 19
-    print 'P[i]', type(P[i]), P[i]
-    print 'T[i]', type(T[i]), T[i]
-    print 'r[i]', type(r[i]), r[i]
-    print 'M', type(M), M
-    P[i+1] = P[i]*M/(T[i]*r[i]**2)
-    P[i+1]*(-4*np.pi*G*mu**2*mH**2/k**2)
+#    print i # stops at 19
+#    print 'P[i]', type(P[i]), P[i]
+#    print 'T[i]', type(T[i]), T[i]
+#    print 'r[i]', type(r[i]), r[i]
+#    print 'M', type(M), M
+    P[i+1] = (P[i] * M) / (T[i] * r[i]**2)
+    P[i+1]*( -4 * np.pi * G * (mu * mH / k)**2 )
     if T[i] > T[0]*0.5:
         T[i+1] = (adia/(P[i+1]**(1-gamma)))**(1/gamma)
     else: 
         T[i+1] = T[i]
-    M += (r[i+1]-r[i])**2*(P[i+1]/T[i+1])
+    M = M + (dr**2*P[i+1]/T[i+1])
 
 rho = P*mu*mH/k/T
 
