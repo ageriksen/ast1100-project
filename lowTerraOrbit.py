c256dcd1395284aef2b51c503243cf8351cc3463
anderger <andersge92@gmail.com>
2016-11-14 05:50:16 +0100

    last attempt at fixing shit that's wrong. No idea, just gonna have to show what little i've got an hope I don't flunk.

--- lowTerraOrbit.py
+++ lowTerraOrbit.py
@@ -23,6 +23,36 @@ import scipy.interpolate as scp
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+def set_axes_equal(ax):
+        '''
+        Make axes of 3D plot have equal scale so that spheres appear as spheres,
+        cubes as cubes, etc..  This is one possible solution to Matplotlib's
+        ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+        Input
+        ax: a matplotlib axis, e.g., as output from plt.gca().
+        '''
+
+        x_limits = ax.get_xlim3d()
+        y_limits = ax.get_ylim3d()
+        z_limits = ax.get_zlim3d()
+
+        x_range = abs(x_limits[1] - x_limits[0])
+        x_middle = np.mean(x_limits)
+        y_range = abs(y_limits[1] - y_limits[0])
+        y_middle = np.mean(y_limits)
+        z_range = abs(z_limits[1] - z_limits[0])
+        z_middle = np.mean(z_limits)
+
+        # The plot bounding box is a sphere in the sense of the infinity
+        # norm, hence I call half the max range the plot radius.
+        plot_radius = 0.5*max([x_range, y_range, z_range])
+
+        ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+        ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+        ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
+
 # constants
 G = 6.674e-11 #N * m^2 * kg^-2 gravitational constant
 AU = 149597871e3 #m in an AU
@@ -37,6 +67,8 @@ path = '/home/anderger/Documents/ast1100-project/atmosphere_val/'
 T = np.load(path+'Temperature.npy')# Temperature array as a function of distance
 rho = np.load(path+'Density.npy') # temperature array as func of distance
 r_dense = np.load(path+'Position.npy') # position vector for density
+#rho = rho[::-1]
+#r_dense = r_dense[::-1]
 rho_func = scp.interpolate.interp1d(r_dense, rho)
 
 # radius, mass from AST1100solarsystem
@@ -53,7 +85,7 @@ print 'r0', type(r0), r0
 v0 = np.array((0.00786262094791 ,  2985.69804027 ,  0))
 print 'v0', type(v0), v0
 
-nr = 100000
+nr = 500000
 r = np.zeros((nr, 3))
 v = np.zeros((nr, 3))
 r[0] = r0
@@ -62,86 +94,80 @@ print 'r0', r[0]
 print 'v0', v[0]
 
 #Leapfrog integration
-t0 = 1 #s after init call, 
-dt = 60#s timestep
-t = 0
-first = 0
-second = 0
+dt = 10#s timestep
+t = 1 # seconds after "init" call
+r_min = 0
+
 # v0 to vhalf:
 r_ = np.linalg.norm(r[0]) 
 a = -G* ( M/r_**3 ) * r[0]
 v[0] = v[0] + 0.5*a*dt
-print 'initial velocity' 
-print v[0]
 
 print 'integrating'
 print '===================================='
 # integrater
-for i in range(nr-1):
-    Dv = 0
-    # checking for nan type objects
-    if any(np.isnan(r[i])):
-        print 'error in pos'
-        print 'here is the error, at time ', t, 'index ', i
-        print 'at a distance ', r[i+1]
-        break
-    if any(np.isnan(v[i])):
-        print 'error in vel'
-        print 'here is the error, at time ', t, 'index ', i
-        print 'at a distance ', r[i+1]
-        break
-
-    #updating position
+for i in range( nr - 1 ):
     r[i+1] = r[i] + v[i]*dt
-
-    #position reliant variables
     r_ = np.linalg.norm(r[i+1])
     v_ = np.linalg.norm(v[i])
-    ag = -G*r[i+1]*M/r_**3
-
-    # checks to include drag
-    if r_ < r_dense[-1]:
-        ad = (-0.5*rho[i]*v[i]*A*v_**3)/m
-        ratio = np.linalg.norm(ag)/np.linalg.norm(ad)
-        if ratio < 1000:
-            if first < 1:
-                print 'too low'
-                first = 1
-            a = ag + ad
-        else: 
-            if first < 1:
-                print 'still high enough'
-                first = 1
-            a = ag
-    else: 
-        if second < 1:
-            print 'we use only gravity'
-            print ag
-            second = 1
-        a = ag
-
-    # checks to boost 
+    a = -G*r[i+1]*M*r_**(-3)
+    v_so = np.sqrt( (M*G)/(r_) )
+    theta = np.arctan2(r[i+1, 0], r[i+1, 1])
+    Dv = 0
     e_v = (v[i]/v_)
-    if -5e4 < r[i+1,1] < 5e4 and 0 < r[i+1,0]: 
-        dv = -0.3*v_
-        #th = np.arctan2(v[i,0], v[i,1])
-        #Dv = np.zeros(3)
-        #Dv[0] = dv*np.cos(th)
-        #Dv[1] = dv*np.sin(th)
-        Dv = dv*e_v
-    elif -5e4 < r[i+1,1] < 5e4 and r[i+1,0] < 0: 
-        v_so = np.sqrt( (M * G) / (r_) )
-        dv = v_so - v_
-        #th = np.arctan2(v[i,0], v[i,1])
-        #Dv = np.zeros(3)
-        #Dv[0] = dv*np.cos(th)
-        #Dv[1] = dv*np.sin(th)
-        Dv = dv*(v[i]/v_)
+    # at 1 extreme, lower vel. 
+    # 1/2 round later, return to stable 
+    # orbit velocity. 
+    if r_min == 0:
+        if abs(theta) < np.pi/20:
+            Dv = -0.01*v_*e_v
+        elif abs(theta - np.pi) < np.pi/20:
+            dv = v_so - v_
+            Dv = dv*e_v
+    elif r_min != 0:
+        if abs(theta) < np.pi/20:
+            rxv = np.cross(r[i+1], v[i])
+            e_rxv = rxv/(np.linalg.norm(rxv))
+            Dv = 0.01*v_*e_rxv
+    
+    if r_ <= r_dense[-1]:
+        ii = np.nonzero(np.in1d(r_dense ,  r_))
+        if type(ii[0]) == int:
+            rh = rho[ii[0]]
+            ad = 0.5*rh*A*v_**2*m**(-1)
+            ratio = ag / ad
+    else: 
+        ratio = 10000
 
-    # estimating new velocity
-    v[i+1] = v[i] + a*dt + Dv
 
-    # updating times and indexes to record last entry.
+    if ratio <= 3000:
+        r_min = r_
+    elif ratio < 1000:
+        print 'too low again.' 
+        print 'index', i
+        break
+
+
+    if ratio > 3000:
+        if abs(theta) < np.pi/12:
+            Dv = -0.01*v_*e_v
+        elif abs(theta - np.pi) < np.pi/8:
+            print 'hey ho'
+            dv = v_so - v_
+            Dv = dv*e_v
+        v[i+1] = v[i] + a*dt + Dv
+    elif ratio <= 3000:
+        if abs(theta) < np.pi/12:
+           rxv = np.cross(r[i+1], v[i])
+           print 'r x v: ', rxv
+           e_rxv = rxv / np.linalg.norm(rxv)
+           Dv = 0.01*v_*e_v
+        v[i+1] = v[i] + a*dt + Dv
+        v1_ = np.linalg.norm(v[i+1])
+        if v1_ != v_so:
+           dv = v_so - v1_
+           e_v1 = v[i+1]/v1_
+           v[i+1] = v[i+1] + dv*e_v1
     end = i
     t += dt
 
@@ -150,15 +176,18 @@ print 'at this time, index ', end, ', position and velocity are:'
 print 'r', r[end]
 print 'v', v[end]
 
+absV = np.linalg.norm(v, axis=1)
+absR = np.linalg.norm(r, axis=1)
+print 'absR', np.shape(absR)
+
+#======================================================================#
 ## making a 3D plot of orbital path
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-ax.set_aspect("equal")
 #hold('on')
 
 #initial position of satelite:
-print 'now to plot the shit'
-#ax.plot([r[end,0]], [r[end,1]], [r[end,2]], color='g', marker='o', label='end')
+ax.plot([r[end,0]], [r[end,1]], [r[end,2]], color='g', marker='o', label='end')
 ax.plot([r[0,0]], [r[0,1]], [r[0,2]], color='g', marker='o', label='beginning')
 
 ##plotting a sphere for the planet.
@@ -171,28 +200,16 @@ ax.plot_wireframe(x, y, z, color="r")
 #plotting satellite position
 ax.plot(r[:,0], r[:,1], r[:,2], label='satellite')
 
-# plotting vectors for points of boost
-#ax.quiver(v_boost)
-
 ax.legend()
+set_axes_equal(ax)
+
+# plotting 2D ===================
+plt.figure()
+plt.plot(absR, label='absolute distance')
+plt.legend()
+
+plt.figure()
+plt.plot(absV, label='absolute velocity')
+plt.legend()
+
 plt.show()
-
-
-"""
-for testing: 
-
-    print 'at time ', t, ', index ', i, ' we need send in:'
-    print 'r(',t,'): ', r[i]
-    print 'v: ', v[i]
-    print 'dt: ', dt
-    print 'norm r', np.linalg.norm(r[i])
-    print 'Dv: ', v[i]*dt
-
-
-    print 'r[i+1]', r[i+1]
-    print 'new norm', r_
-
-    print 'with grav. acceleration', ag
-
-    print 'acceleration is: ', a
-"""
